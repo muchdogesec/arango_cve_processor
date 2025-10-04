72f5b69fdb901aa05e89d010a3e3e8143cd5e40c
Fadl <chaos@efqr.dev>
2025-10-04 16:52:30 +0100

    eliminate index ambiguity

--- arango_cve_processor/managers/cve_capec.py
+++ arango_cve_processor/managers/cve_capec.py
@@ -40,7 +40,7 @@ class CveCapec(CveCwe, relationship_note="cve-capec"):
         if not vuln_map:
             return None
         rel_query = """
-        FOR doc IN @@edge_collection // uses acvep-capec-attack
+        FOR doc IN @@edge_collection  OPTIONS {indexHint: "acvep-capec-attack", forceIndexHint: true}
         FILTER doc._arango_cve_processor_note == @cve_cwe_note
                 AND doc.target_ref IN @vuln_ids
                 AND doc._is_latest == TRUE AND doc._is_ref != true
